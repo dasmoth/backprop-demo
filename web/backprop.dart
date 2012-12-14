70b3a365ff9dba2730a6249f2725d596591b8c37
Thomas Down <thomas.a.down@googlemail.com>
2012-12-14 22:05:20 +0000

    first commit

---
+++ web/backprop.dart
@@ -0,0 +1,213 @@
+part of xordemo;
+
+double logit(double x) {
+  return 1.0 / (1.0+exp(-x));
+}
+
+class Matrix {
+  List<double> values;
+  final int rows;
+  final int columns;
+  
+  Matrix(int this.rows, int this.columns) {
+    values = new List<double>(rows*columns);
+  }
+  
+  double get(int r, int c) {
+    return values[(r*columns) + c];
+  }
+  
+  void set(int r, int c, double v) {
+    values[(r*columns) + c] = v;
+  }
+}
+
+abstract class Layer {
+  static final Random rng = new Random(); 
+  
+  Matrix weights;
+  List<double> values;
+  int minNode, maxNode;
+  
+  Layer(int nodes, bool hasBias) {
+    minNode = hasBias ? 0 : 1;
+    maxNode = nodes;
+    values = new List<double>(maxNode+1);
+    for (int n = minNode; n <= maxNode; ++n) {
+      values[n] = (n==0) ? 1.0 : 0.0;
+    }
+  }
+}
+
+class InputLayer extends Layer {
+  HiddenLayer nextLayer;
+  Matrix weights;
+  Matrix weightChanges;
+  
+  InputLayer(int nodes, HiddenLayer this.nextLayer) : super(nodes, true){
+    weights = new Matrix(maxNode + 1, nextLayer.maxNode + 1);
+    weightChanges = new Matrix(maxNode + 1, nextLayer.maxNode + 1);
+    
+    for (int own = minNode; own <= maxNode; ++own) {
+      for (int other = nextLayer.minNode; other <= nextLayer.maxNode; ++other) {
+        weights.set(own, other, (4.0*Layer.rng.nextDouble())-2);
+        weightChanges.set(own, other, (4.0*Layer.rng.nextDouble())-2);
+      }
+    }
+  }
+  
+  void update(List<double> inputs) {
+    for (int n = 0; n < inputs.length; ++n) {
+      values[n+1] = inputs[n];
+    }
+    // print('Updated inputs: ${values}');
+    nextLayer.update(this);
+  }
+  
+  void backpropagate(List<double> outputs, double rate, double momentum) {
+    List<double> nextErrors = nextLayer.backpropagate(outputs, rate, momentum);
+    for (int n = minNode; n <= maxNode; ++n) {
+      for (int m = nextLayer.minNode; m <= nextLayer.maxNode; ++m) {
+        double errorDeriv = nextErrors[m] * values[n];
+        double delta = rate * errorDeriv + momentum * weightChanges.get(n, m);
+        weights.set(n, m, weights.get(n, m) + delta);
+        weightChanges.set(n,m, delta);
+      }
+    }
+  }
+}
+
+class HiddenLayer extends Layer {
+  OutputLayer nextLayer;
+  Matrix weights;
+  Matrix weightChanges;
+  
+  HiddenLayer(int nodes, OutputLayer this.nextLayer) : super(nodes, true) {
+    weights = new Matrix(maxNode + 1, nextLayer.maxNode + 1);
+    weightChanges = new Matrix(maxNode + 1, nextLayer.maxNode + 1);
+    
+    for (int own = minNode; own <= maxNode; ++own) {
+      for (int other = nextLayer.minNode; other <= nextLayer.maxNode; ++other) {
+        weights.set(own, other, (4.0*Layer.rng.nextDouble())-2);
+        weightChanges.set(own, other, (4.0*Layer.rng.nextDouble())-2);
+      }
+    }
+  }
+  
+  void update(Layer prev) {
+    for (int n = 1; n <= maxNode; ++n) {
+      double sum = 0.0;
+      for (int m = prev.minNode; m <= prev.maxNode; ++m) {
+        sum += prev.values[m] * prev.weights.get(m, n);
+      }
+      values[n] = logit(sum);
+    }
+    // print('Updated hidden: ${values}');
+    nextLayer.update(this);
+  }
+  
+  List<double> backpropagate(List<double> outputs, double rate, double momentum) {
+    List<double> nextErrors = nextLayer.backpropagate(outputs, rate, momentum);
+    for (int n = minNode; n <= maxNode; ++n) {
+      for (int m = nextLayer.minNode; m <= nextLayer.maxNode; ++m) {
+        double errorDeriv = nextErrors[m] * values[n];
+        double delta = rate * errorDeriv + momentum * weightChanges.get(n, m);
+        weights.set(n, m, weights.get(n, m) + delta);
+        weightChanges.set(n,m, delta);
+      }
+    }
+    
+    List<double> errors = new List<double>(maxNode + 1);
+    for (int n = minNode; n <= maxNode; ++n) {
+      double prevError = 0.0;
+      for (int m = nextLayer.minNode; m <= nextLayer.maxNode; ++m) {
+        prevError += nextErrors[m] * weights.get(n, m);
+      }
+      double corrFactor = values[n] * (1-values[n]);
+      errors[n] = prevError * corrFactor;
+    }
+    
+    return errors;
+  }
+}
+
+class OutputLayer extends Layer {
+  OutputLayer(int nodes) : super(nodes, false) {
+  }
+  
+  void update(Layer prev) {
+    for (int n = 1; n <= maxNode; ++n) {
+      double sum = 0.0;
+      for (int m = prev.minNode; m <= prev.maxNode; ++m) {
+        sum += prev.values[m] * prev.weights.get(m, n);
+      }
+      values[n] = logit(sum);
+    }
+    // print('Updated outputs: ${values}');
+  }
+
+  List<double> backpropagate(List<double> outputs, double rate, double momentum) {
+    List<double> errors = new List<double>(maxNode + 1);
+    for (int n = minNode; n <= maxNode; ++n) {
+      double calc = values[n], real = outputs[n-1];
+      errors[n] = ((real-calc) * calc * (1-calc));
+    }
+    
+    return errors;
+  }
+  
+  double getError(double real, int n) {
+    double calc = values[n];
+    return 0.5 * pow(real-calc, 2);
+  }
+}
+
+class Example {
+  List<double> inputs;
+  List<double> outputs;
+
+  Example(List<double> this.inputs, List<double> this.outputs);
+}
+
+class BasicNetwork {
+  InputLayer input;
+  HiddenLayer hidden;
+  OutputLayer output;
+  int numIterations = 0;
+  
+  BasicNetwork(InputLayer this.input, HiddenLayer this.hidden, OutputLayer this.output);
+  
+  void train(List<Example> trainingExamples, List<Example> testExamples, double rate, double momentum) {
+    int numIterations = 0;
+    while (true) {
+      trainStep(trainingExamples, testExamples, rate, momentum);
+      
+      if (numIterations > 1000) {
+        break;
+      }
+    }
+  }
+  
+  void trainStep(List<Example> trainingExamples, List<Example> testExamples, double rate, double momentum) {
+      ++numIterations;
+      double trainingError = 0.0;
+      for (Example e in trainingExamples) {
+        input.update(e.inputs);
+        input.backpropagate(e.outputs, rate, momentum);
+        
+        for (int n = 0; n < e.outputs.length; ++n) {
+          trainingError += output.getError(e.outputs[n], n+1);
+        }
+      }
+      
+      double validationError = 0.0;
+      for (Example e in testExamples) {
+        input.update(e.inputs);
+        for (int n = 0; n < e.outputs.length; ++n) {
+          validationError += output.getError(e.outputs[n], n+1);
+        }
+      }
+      
+      print('trainError=$trainingError; validationEror=$validationError.');
+  }
+}
---
+++ web/xordemo.dart
@@ -0,0 +1,112 @@
+library xordemo;
+
+import 'dart:html';
+import 'dart:math';
+
+part 'backprop.dart';
+
+class ViewerRow {
+  List inputCells = [];
+  List truthCells = [];
+  List outputCells = [];
+  var row;
+  
+  ViewerRow(int inputs, int outputs) {
+    row = new TableRowElement();
+    
+    for (int i = 0; i < inputs; ++i) {
+      var c = new TableCellElement();
+      inputCells.add(c);
+      row.elements.add(c);
+    }
+    row.nodes.add(new TableCellElement());    
+    for (int i = 0; i < outputs; ++i) {
+      var c = new TableCellElement();
+      truthCells.add(c);
+      row.elements.add(c);
+    }
+    
+    for (int i = 0; i < outputs; ++i) {
+      var c = new TableCellElement();
+      outputCells.add(c);
+      row.elements.add(c);
+    }
+  }
+}
+
+List<Example> examples = <Example>[new Example([0.0,0.0], [0.0]),
+                                   new Example([0.0,1.0], [1.0]),
+                                   new Example([1.0,0.0], [1.0]),
+                                   new Example([1.0,1.0], [0.0])];
+List<ViewerRow> viewerRows = new List<ViewerRow>(examples.length);
+
+
+double clamp(double min, double x, double max) {
+  if (x < min) {
+    return min;
+  } else if (x > max) {
+    return  max;
+  } else {
+    return x;
+  }
+}
+
+void colorCell(Element e, double score) {
+  int c = (clamp(0.0, 1.0-score, 1.0) * 255).toInt();
+  e.style.backgroundColor = 'rgb($c,$c,$c)';
+}
+
+void colorCells(List<Element> el, List<double> scores) {
+  for (int i = 0; i < el.length; ++i) {
+    colorCell(el[i], scores[i]);
+  }
+}
+
+void main() {
+  Element viewer = query('#viewer');
+  ViewerRow header = new ViewerRow(2, 1);
+  for (int i = 0; i < header.inputCells.length; ++i) {
+    header.inputCells[i].text = 'I$i';
+  }
+  for (int i = 0; i < header.truthCells.length; ++i) {
+    header.truthCells[i].text = 'T$i';
+  }
+  for (int i = 0; i < header.outputCells.length; ++i) {
+    header.outputCells[i].text = 'O$i';
+  }
+  viewer.elements.add(header.row);
+  
+  for (int ei = 0; ei < examples.length; ++ei) {
+    Example e = examples[ei];
+    ViewerRow r = new ViewerRow(2, 1);
+    colorCells(r.inputCells, e.inputs);
+    colorCells(r.truthCells, e.outputs);
+    viewer.elements.add(r.row);
+    viewerRows[ei] = r;
+  }
+  
+  query("#text")
+    ..text = "Click to start training"
+    ..on.click.add(nnTest);
+}
+
+BasicNetwork network;
+
+void nnTest(Event event) {
+  OutputLayer output = new OutputLayer(1);
+  HiddenLayer hidden = new HiddenLayer(3, output);
+  InputLayer input = new InputLayer(2, hidden);
+  network = new BasicNetwork(input, hidden, output);
+  
+  window.requestAnimationFrame(tick);
+}
+
+void tick(num time) {
+  network.trainStep(examples, [], 0.5, 0.1);
+  for (int i = 0; i < examples.length; ++i) {
+    Example e = examples[i];
+    network.input.update(e.inputs);
+    colorCells(viewerRows[i].outputCells, network.output.values.getRange(1, e.outputs.length));
+  }
+  window.requestAnimationFrame(tick);
+}
